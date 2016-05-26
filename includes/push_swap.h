e47edc4903698d5782e35dbde6ce5bc68366f100
Arthur Dompe <adompe@student.42.fr>
2016-05-26 14:18:08 +0200

    add prev strucu

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -23,6 +23,7 @@ typedef struct          s_stack
 {
 	int                	content;
 	struct s_stack      *next;
+	struct s_stack		*prev;
 }                       t_stack;
 
 typedef struct			s_push_swap
